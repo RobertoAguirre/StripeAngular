16f032b5ff57290dd6e68f0b23a6e502b11bd1db
RobertoAguirre <roberto.aguirre.rodriguez@gmail.com>
2024-06-05 10:22:05 -0600

    se agrega consumo de backend

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angula
 import { loadStripe, Stripe, StripeCardElement } from '@stripe/stripe-js';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,HttpClientModule],
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
@@ -17,10 +18,10 @@ export class PaymentComponent implements OnInit, AfterViewInit {
   card!: StripeCardElement;
   stripeTest!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, public http:HttpClient) {}
 
   async ngOnInit() {
-    const stripe = await loadStripe('LLAVE PUBLICA AQUI'); // Reemplaza 'TU_LLAVE_PUBLICA_STRIPE' con tu llave pública de Stripe
+    const stripe = await loadStripe('pk_test_51MfpNjI2RlxJ9P4GNVq5kaockeoHSVCfFt3C56hSzfD2CqcSO9DxH0zPZHHlKG8jIWEoETLMHwbc3bLs3LUuTNIn00ekEGLwgM'); // Reemplaza 'TU_LLAVE_PUBLICA_STRIPE' con tu llave pública de Stripe
     if (!stripe) {
       console.error('Stripe no se ha podido cargar.');
       return;
@@ -51,6 +52,16 @@ export class PaymentComponent implements OnInit, AfterViewInit {
     if (token) {
       // Envía el token al servidor
       console.log(token);
+      this.http.post('http://localhost:3000/api/payment', { token: token.id }).subscribe(
+        response => {
+          console.log('Payment successful!', response);
+        },
+        error => {
+          console.error('Payment error:', error);
+        }
+      );
+
+
     } else if (error) {
       // Maneja el error aquí
       console.log(error.message);
